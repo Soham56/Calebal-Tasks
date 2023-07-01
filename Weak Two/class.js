c76e4dbd7a77079e47301b0ee4ede574a2f8d677
Soham <sohamdey567@gmail.com>
2023-07-01 13:34:23 +0530

    uploaded new files

---
+++ Weak Two/class.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+
+// CRUD operation
+
+// Create File
+
+/*
+Synchrous
+
+try {
+    fs.appendFileSync("file3.txt"," Hello world");
+    console.log("success");
+} catch (error) {
+    e = new Error(error);
+    console.log(e);
+}
+*/
+
+/*
+
+Asynchronous
+
+fs.appendFile('file3.txt',"Hey what's up", (e)=>{
+    if(e) throw e;
+
+    console.log("success");
+})
+
+*/ 
+
+/*
+
+Asynchronous
+
+fs.unlink("file2.txt",(e)=>{
+    if(e) throw e;
+    console.log("Ha ha ha, go to hell 👹");
+})
+
+*/
+
+try{
+    fs.unlinkSync("file1.txt");
+    console.log("Deleted Successfully");
+}
+catch(e){
+    console.log(e);
+}
+
+console.log("Heelo");
