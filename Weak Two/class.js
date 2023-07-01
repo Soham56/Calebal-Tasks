2fcdcd02b4f0c3e9ffd078484c053dd6591fdfcb
Soham <sohamdey567@gmail.com>
2023-07-01 13:35:44 +0530

    files uploaded

--- Weak Two/class.js
+++ Weak Two/class.js
@@ -14,6 +14,15 @@ try {
     e = new Error(error);
     console.log(e);
 }
+
+try {
+    fs.writeFileSync("file3.txt"," Hello world");
+    console.log("success");
+} catch (error) {
+    e = new Error(error);
+    console.log(e);
+}
+
 */
 
 /*
@@ -21,6 +30,12 @@ try {
 Asynchronous
 
 fs.appendFile('file3.txt',"Hey what's up", (e)=>{
+    if(e) throw e;
+
+    console.log("success");
+})
+
+fs.writeFile('file3.txt',"Hey what's up", (e)=>{
     if(e) throw e;
 
     console.log("success");
@@ -37,14 +52,20 @@ fs.unlink("file2.txt",(e)=>{
     console.log("Ha ha ha, go to hell 👹");
 })
 
+fs.unlink("file2.txt",(e)=>{
+    if(e) throw e;
+    console.log("Ha ha ha, go to hell 👹");
+})
+
 */
 
-try{
-    fs.unlinkSync("file1.txt");
-    console.log("Deleted Successfully");
-}
-catch(e){
-    console.log(e);
-}
+// try{
+//     fs.unlinkSync("file1.txt");
+//     console.log("Deleted Successfully");
+// }
+// catch(e){
+//     console.log(e);
+// }
 
-console.log("Heelo");
+// console.log("Heelo");
+
